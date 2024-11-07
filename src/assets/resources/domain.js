8d05735b6fcdb9105c42d4f418fac2283f91c0a7
노경열 <sikadeer105@gmail.com>
2024-11-08 02:03:27 +0900

    #

--- src/assets/resources/domain.js
+++ src/assets/resources/domain.js
@@ -1,5 +1,5 @@
-//const DOMAIN =
-//  "https://port-0-globe-server-m35fhr4yb9336d48.sel4.cloudtype.app";
-const DOMAIN = "http://localhost:3000";
+const DOMAIN =
+  "https://port-0-globe-server-m35fhr4yb9336d48.sel4.cloudtype.app";
+//const DOMAIN = "http://localhost:3000";
 
 export default DOMAIN;
